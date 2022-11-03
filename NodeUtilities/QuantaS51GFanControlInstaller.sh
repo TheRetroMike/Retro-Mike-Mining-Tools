d21c81c59142b0d85974626d2c1c56bf02b0522c
Michael Dlesk <michaeldlesk@gmail.com>
2022-11-02 20:28:47 -0500

    Create QuantaS51GFanControlInstaller.sh

---
+++ NodeUtilities/QuantaS51GFanControlInstaller.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget -O /QuantaS51GFanControl.sh https://raw.githubusercontent.com/TheRetroMike/Retro-Mike-Mining-Tools/main/NodeUtilities/QuantaS51GFanControl.sh
+chmod +x /QuantaS51GFanControl.sh
+
+cat >/etc/systemd/system/s51gfancontrol.service<<'EOF'
+[Unit]
+Description=Start Retro Mike's Quanta S51G fan control service
+StartLimitIntervalSec=0
+
+[Service]
+Type=idle
+ExecStart=/QuantaS51GFanControl.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable s51gfancontrol
